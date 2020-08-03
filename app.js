d02004e214bd9f3eb555b0ef29bd77405c4ed1b8
PelumiWeb <Pelumiogundipe905@gmail.com>
2020-08-04 00:36:52 +0100

    making it sure it worked

--- app.js
+++ app.js
@@ -1,18 +1,15 @@
-const path = require('path')
-const express = require('express')
-const app = express()
+const path = require("path");
+const express = require("express");
+const app = express();
 
-const port  = process.env.PORT || 5000
-const source = path.join(__dirname, './src/')
-console.log(source)
- app.use(express.static(source))
-
+const port = process.env.PORT || 5000;
+// const source = path.join(__dirname, './src/')
+console.log(source);
+app.use(express.static(source));
 
 app.listen(port, () => {
-    console.log('Server is up on port ' + port)
-})
-
-
+  console.log("Server is up on port " + port);
+});
 
 // if (process.env.NODE_ENV === 'production') {
 // 	app.use(express.static('client/build'));
