9609dc989dff86b3511014ffcbc0d2787d4c3c20
PelumiWeb <Pelumiogundipe905@gmail.com>
2020-08-29 23:39:35 -0700

    making sure it works

--- app.js
+++ app.js
@@ -6,16 +6,16 @@ const port = process.env.PORT || 5000;
 const source = path.join(__dirname, "public");
 console.log(source);
 app.use(express.static(source));
-app.get("*", (req, res) => {
-  res.send("public", "Index.html");
-});
-
-// if (process.env.NODE_ENV === "production") {
-//   app.use(express.static("client/build"));
-// }
-// app.get("*", (request, response) => {
-//   response.sendFile(path.join(__dirname, "client/build", "index.html"));
+// app.get("*", (req, res) => {
+//   res.send("public", "Index.html");
 // });
 app.listen(port, () => {
   console.log("Server is up on port " + port);
 });
+
+// if (process.env.NODE_ENV === 'production') {
+// 	app.use(express.static('client/build'));
+// }
+// app.get('*', (request, response) => {
+// 	response.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+// });
